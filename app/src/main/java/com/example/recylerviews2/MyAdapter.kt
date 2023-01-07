e711537c9b088b82c25734b9debfc94a94cac2da
Emmanuel-Rono <ronoemmanuel933@gmail.com>
2023-01-07 21:15:05 +0300

    Proceeding

---
+++ app/src/main/java/com/example/recylerviews2/MyAdapter.kt
@@ -0,0 +1,38 @@
+package com.example.recylerviews2
+
+import android.os.Parcel
+import android.os.Parcelable
+import android.view.LayoutInflater
+import android.view.View
+import android.view.View.inflate
+import android.view.ViewGroup
+import android.view.ViewParent
+import androidx.appcompat.resources.Compatibility.Api21Impl.inflate
+import androidx.recyclerview.widget.RecyclerView
+
+class MyAdapter(private val newList:Array<dataClass>): RecyclerView.Adapter<MyAdapter.MyviewHolder>()
+
+
+{
+        class MyviewHolder(Itemview:View):RecyclerView.ViewHolder(Itemview)
+        {
+                //Here we gett the views
+
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyviewHolder {
+                val view = LayoutInflater.from(parent.context)
+                        .inflate(R.layout.recylayout, parent, false)
+                return MyviewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: MyviewHolder, position: Int) {
+                TODO("Not yet implemented")
+        }
+
+        override fun getItemCount(): Int {
+                return newList.size
+        }
+
+
+}
---
+++ app/src/main/java/com/example/recylerviews2/dataClass.kt
@@ -0,0 +1,3 @@
+package com.example.recylerviews2
+
+data class dataClass(var image:Int, var heading :String)
