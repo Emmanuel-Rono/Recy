20702f0d39c6754154a094d45ecc617e16a04015
Emmanuel-Rono <ronoemmanuel933@gmail.com>
2023-01-08 19:33:25 +0300

    Proceeding

--- app/src/main/java/com/example/recylerviews2/MyAdapter.kt
+++ app/src/main/java/com/example/recylerviews2/MyAdapter.kt
@@ -1,41 +1,39 @@
 package com.example.recylerviews2
 
-import android.os.Parcel
-import android.os.Parcelable
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
-import android.view.View.inflate
 import android.view.ViewGroup
-import android.view.ViewParent
+import android.widget.ImageView
 import android.widget.TextView
-import androidx.appcompat.resources.Compatibility.Api21Impl.inflate
 import androidx.recyclerview.widget.RecyclerView
 
-class MyAdapter(private val newList:Array<dataClass>): RecyclerView.Adapter<MyAdapter.MyviewHolder>()
-
-
+class MyAdapter(val context:Context, val newList:Array<dataClass>):
+        RecyclerView.Adapter<RecyclerView.ViewHolder>()
 {
-        class MyviewHolder(Itemview:View):RecyclerView.ViewHolder(Itemview)
-        {
-                //Here we gett the views
-                val textdata=Itemview.findById<TextView>(R.id.TextViewdata)
-                val textdata=Itemview.findViewById<TextView>(R.id.Text)
-                val data2
-
-        }
-
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyviewHolder {
                 val view = LayoutInflater.from(parent.context)
                         .inflate(R.layout.recylayout, parent, false)
                 return MyviewHolder(view)
         }
 
-        override fun onBindViewHolder(holder: MyviewHolder, position: Int) {
-                TODO("Not yet implemented")
-        }
 
         override fun getItemCount(): Int {
                 return newList.size
+        }
+        class MyviewHolder(Itemview:View):RecyclerView.ViewHolder(Itemview)
+        {
+                val Textdata2=Itemview.findViewById<TextView>(R.id.textName_new)
+                val Imagetwo=Itemview.findViewById<ImageView>(R.id.image2)
+        }
+        class newViewHolder(Itemview:View):RecyclerView.ViewHolder(Itemview)
+        {
+                val Textdata1=Itemview.findViewById<TextView>(R.id.textName)
+                val ImageOne=Itemview.findViewById<ImageView>(R.id.image1)
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+                TODO("Not yet implemented")
         }
 
 
